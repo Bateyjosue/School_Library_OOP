389d2da1f1fe53a5d70e6cfe90b37a03f7113a18
Bateyjosue <josuebatey19@gmail.com>
2022-11-08 07:58:29 +0200

    fix Linter error

--- person.rb
+++ person.rb
@@ -1,26 +1,21 @@
-
 class Person
-  def initialize(name = 'Uhnknown', age, parent_permission = true)
-    @id = Random.rand(1...10000)
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1...10_000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  attr_accessor : id
-  attr_accessor : name
-  attr_accessor : age
+  private
 
-  def is_of_age?
-    if @age >= 18
-      true
-    else
-      false
-    end
+  def of_age?
+    @age >= 18
   end
 
   def can_use_services?
     true if is_of_age? || @parent_permission
   end
-
 end
--- teacher.rb
+++ teacher.rb
@@ -1,11 +1,12 @@
-require './person.rb'
+require './person'
 
 class Teacher < Person
   def initialize(name, age, specialization)
-    super(name, age, parent_permission)
+    super(age, name, parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
     true
+  end
 end
